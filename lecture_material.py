35264407b368cf6acae158b9bd41def47edbd4dc
Mohammad Nasar Azaz <nasarazazs24ultra@gmail.com>
2025-10-18 12:43:48 +0300

    Initial commit

---
+++ lecture_material.py
@@ -0,0 +1,171 @@
+# #print("hello world")
+#
+# # ex_var = 5
+# #print(ex_var)
+# ex_var1 = 33
+# print(ex_var1)
+#
+# ex_var = 5
+# print(ex_var)
+#
+# exp_two = 2**3
+# print(exp_two)
+#
+# div_10_3 = 10/4
+# # print(div_10_3)
+# #
+# # floor_10_3 = 10//4
+# # print(floor_10_3)
+#
+#
+# ##########################################
+#
+# # #math operator
+# # x =15/3*2*2-(3-4)
+# #
+# # print(x)
+#
+#
+# #######################################################
+#
+# Sum =0
+# sum = 2+4
+# print(sum)
+#
+# #######################################################
+#
+# Sub =0
+#
+# sub =4-2
+# print(sub)
+#
+# #######################################################
+#
+# Mul = 2*4
+# print(Mul)
+#
+# floor_var = 10//3
+# print(floor_var)
+
+
+#
+# example_1 = 3.14159
+# example_2 = 24601
+# example_3 = True
+#
+#
+# print(example_1)
+# print(example_2)
+# print(example_3)
+#
+#
+# print("hello")
+# print((5+5)*3)
+
+# print(1.23 + 2.80)
+#
+# print((123+280)/ 100)
+# print(round(1.23 + 2.80,2))
+#
+# ex3= 1.23+ 2.80;
+# print(ex3);
+# print(round(ex3,2));
+#
+# str_1 = " Hello this is my python code "
+# str_2 = " this is very good language"
+# str_3 = "01234567"
+#
+# print(str_1)
+# print(str_2)
+# print(str_3)
+#
+#
+# str_4 = "Orange"
+# print(str_4[4])
+#
+# # slicing
+# str_5 = "nasarazaz"
+# print(str_5[:3])
+# print(str_5[2:5])
+# print(str_5[4:])
+#
+#
+# # concat
+#
+# # print("Python"+ " :" + "is best programming language" )
+#
+#
+# str_6 = "Just do it!"
+# # print(str_6)
+#
+# print(str_6[10])
+# print(str_6[5:7])
+#
+#
+# str_7 = str_6[4:10]
+# print(str_7)
+#
+
+############################################################################################################
+#
+# ex_1= False
+# ex_2= 234
+# ex_3= "abc"
+# ex_4= str(4.24)
+#
+# print(type(ex_1))
+# print(type(ex_2))
+# print(type(ex_3))
+# print(type(ex_4))
+
+########################################################################################
+#escape sequencegryagray
+
+# print("This is \t Mohammad \'Nasar\' Azaz \n I am a developer\\")
+
+#print(" ******* \n  ***** \n   *** \n    *")
+####################################################
+
+
+# name = input("Enter your name: ")
+# print(name)
+# print(type(name))
+
+print("helow" +11)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
